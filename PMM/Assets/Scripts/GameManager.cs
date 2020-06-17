cba84069b6b6610a7fa2cbd46943104092dcb545
Seda Erdogan <sedaerdogan@hotmail.de>
2020-06-18 00:23:17 +0200

    Collactable Milch mit Anzeige hinzugefuegt

---
+++ PMM/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int currentMilk;
+    public Text milkText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddMilk(int milkToAdd)
+    {
+        currentMilk += milkToAdd;
+        milkText.text = "Milk: " + currentMilk;
+    }
+}
