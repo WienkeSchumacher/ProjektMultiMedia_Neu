1df7f988719181414f40493fcc3c05c3ab53aa5f
Seda Erdogan <sedaerdogan@hotmail.de>
2020-06-18 01:09:48 +0200

    Collectables hinzugefuegt mit effect. Musst mir den DMG erklaeren

--- PMM/Assets/Scripts/MilkPickup.cs
+++ PMM/Assets/Scripts/MilkPickup.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class MilkPickup : MonoBehaviour
 {
     public int value;
+
+    public GameObject pickupEffect;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +26,8 @@ public class MilkPickup : MonoBehaviour
         if (other.tag == "Player")
         {
             FindObjectOfType<GameManager>().AddMilk(value);
+
+            Instantiate(pickupEffect, transform.position, transform.rotation);
 
             Destroy(gameObject);
         }
