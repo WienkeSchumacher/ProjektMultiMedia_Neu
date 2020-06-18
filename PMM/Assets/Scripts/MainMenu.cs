e513967d38d76427f68442256f4f692416b45e10
Seda Erdogan <sedaerdogan@hotmail.de>
2020-06-18 17:55:24 +0200

    Start Menu mit Play und Quit erstellt. Quit klappt wenn es auserhalb von Unity ist

---
+++ PMM/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+    //void Start()
+    //{
+    //    startscreen.SetActive(true);
+
+    //}
+
+    //public void DeactivateStartScreen()
+    //{
+    //    startscreen.SetActive(false);
+    //}
+
+
+
+
+
+}
