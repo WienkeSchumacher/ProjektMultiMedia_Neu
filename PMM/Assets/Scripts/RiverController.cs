28b38a0aa8338bf7b6c24816cfec2dc4abec0767
Seda Erdogan <sedaerdogan@hotmail.de>
2020-07-11 05:52:30 +0200

    Umfeld verbessert, Regen Ambiente hinzugefugt, Skybox verschonert, Licht verbessert

--- PMM/Assets/Scripts/RiverController.cs
+++ PMM/Assets/Scripts/RiverController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class RiverController : MonoBehaviour
 {
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Test");
-
-        FindObjectOfType<GameManager>().EndGame();
+        //Debug.Log("Test");
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
     }
 }
